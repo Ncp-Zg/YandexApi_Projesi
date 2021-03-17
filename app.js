4e4460d9e2cac8867ce3cafea3bb1deb49452849
Necip <ncptzgn@gmail.com>
2021-03-17 19:49:28 +0300

    YandexApi

--- app.js
+++ app.js
@@ -8,13 +8,26 @@ function eventListeners(){
     //Change
     document.getElementById("language").onchange = function(){
         //Arayüz işlemleri
+        console.log("event");
+        ui.changeUI();
     }
 }
+
 const translate = new Translate(document.getElementById("word").nodeValue,document.getElementById("language").value)
+const ui = new UI();
 function translateWord(e){
 
+    translate.changeParameters(document.getElementById("word").value,document.getElementById("language").value);
+    translate.translateWord(function(err,response){
+        if(err){
+            // Hata
+            console.log(err);
+        }
+        else{
+            console.log(response);
+        }
 
-    translate.translateWord();
-    
+    });
+
     e.preventDefault();
 }
